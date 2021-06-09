032405f4e6a13aa5ae32f07519b001f685648441
dfgiordano <federico.giordano90@gmail.com>
2021-06-09 15:32:39 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <header>
+        <a href="#">
+            <img src="../assets/spotify.png" alt="spotify_logo">
+        </a>
+    </header>
+</div>
+    
+</template>
+
+<script>
+
+export default {
+    name: "Header",
+}
+</script>
+
+<style lang="scss">
+header {
+    height: 80px;
+    display: flex;
+    align-items: center;
+    background-color: #2E3A46;
+        & img {
+            height: 50px;
+            margin-left: 30px;
+        }  
+} 
+    
+</style>
